fae55c22fa340a734494097d88dee36c9ff4a34d
sgib <sgib@users.noreply.github.com>
2016-04-04 19:29:55 +0100

    Created Bordered Button class
    The class represents a button with title and border in tint colour and
    the background colour changes to tint colour when button is tapped
    (highlighted).
    Works best when changed to type Custom to avoid the ‘tinting’ effects
    that type System provides.

---
+++ Simple Accounts/BorderedButton.swift
@@ -0,0 +1,37 @@
+//
+//  BorderedButton.swift
+//  Simple Accounts
+//
+//  Created by Steven Gibson on 04/04/2016.
+//  Copyright © 2016 Steven Gibson. All rights reserved.
+//
+
+import UIKit
+
+class BorderedButton: UIButton {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.borderWidth = 1.0
+        layer.borderColor = tintColor.CGColor
+        layer.cornerRadius = 5.0
+        clipsToBounds = true
+        contentEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        setTitleColor(tintColor, forState: .Normal)
+        setTitleColor(UIColor.whiteColor(), forState: .Highlighted)
+        setBackgroundImage(UIImage(color: tintColor), forState: .Highlighted)
+    }
+}
+
+
+public extension UIImage {
+    convenience init(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.width)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.init(CGImage: image.CGImage!)
+    }  
+}
--- Simple Accounts/TransactionViewController.swift
+++ Simple Accounts/TransactionViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TransactionViewController: UIViewController {
 
+    @IBOutlet weak var periodButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

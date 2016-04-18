570a1f874f3ce289d7177a6075bb3c1e7e8c9139
sgib <sgib@users.noreply.github.com>
2016-04-18 08:50:12 +0100

    Added Settings Controller
    At the moment this is not really needed, as all I am doing is
    displaying a static ‘credit notice’ with links to where I got the app’s
    icons from.

---
+++ Simple Accounts/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  Simple Accounts
+//
+//  Created by Steven Gibson on 18/04/2016.
+//  Copyright © 2016 Steven Gibson. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
